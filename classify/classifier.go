675c873d438afa6713de632cf4d3611bcb2cf488
Anatolii Poliukhovych <gwan284@gmail.com>
2017-01-16 19:56:05 +0200

    changed email regex to fit RFC2822

--- classify/classifier.go
+++ classify/classifier.go
@@ -12,7 +12,7 @@ const classifiersNum = 10
 
 var (
 	feedbackPeriod = time.Minute
-	emailRegex = regexp.MustCompile(`^(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})$`)
+	emailRegex = regexp.MustCompile(`^(?:[a-z0-9!#$%&'*+/=?^_` + "`" +`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`+ "`" +`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$`)
 )
 
 type feedback struct {
